1e95456f67cffad570ac1de736ad2eb5e43a7ea5
maruf127679-pixel <maruf127679@gmail.com>
2026-09-15 10:59:50 +0600

    Update ownerInfo.js

--- scripts/cmds/ownerInfo.js
+++ scripts/cmds/ownerInfo.js
@@ -20,7 +20,7 @@ module.exports = {
   onStart: async function ({ api, event }) {
     const time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
     const imgPath = __dirname + "/cache/owner_avatar.png";
-    const profilePicURL = "https://graph.facebook.com/100066542686904/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
+    const profilePicURL = "";
 
     const ownerInfo = `
 ┏━━━━━━━━━━━━━━━━━━━━━┓
@@ -29,14 +29,14 @@ module.exports = {
 ┃ 👤 𝐍𝐚𝐦𝐞      : Maruf Hossainッ
 ┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
 ┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : single 🙁
-┃ 🎂 𝐀𝐠𝐞       : 18+
-┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
-┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : SSC 25
+┃ 🎂 𝐀𝐠𝐞       : 
+┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚m
+┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 
 ┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : Meherpur, Bangladesh
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : Srotoshini-স্রোত্বোসিনী
 ┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : ×
-┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : fb.com/profile.php?id=100066542686904
+┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : fb.com/profile.php?id=7283748393889287
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞: ${time}
 ┗━━━━━━━━━━━━━━━━━━━━━┛
