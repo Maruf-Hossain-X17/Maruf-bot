489453a57f5ffc5d880a91e0700fdf8b3849bd03
maruf127679-pixel <maruf127679@gmail.com>
2026-09-15 11:00:45 +0600

    Update pair.js

--- scripts/cmds/pair.js
+++ scripts/cmds/pair.js
@@ -123,8 +123,8 @@ module.exports = {
                         
                         // Custom Percentage Logic Added Here
                         let percentage;
-                        const uidA = "100066542686904";
-                        const uidB = "61570727252463";
+                        const uidA = "10665426838394";
+                        const uidB = "6157839383463";
 
                         if ((String(event.senderID) === uidA && String(selectedMatch.id) === uidB) || 
                             (String(event.senderID) === uidB && String(selectedMatch.id) === uidA)) {
